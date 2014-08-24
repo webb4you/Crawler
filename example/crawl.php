954e8fc32fd05f637d777ab98f56179c5c8f7115
Ilan Rivers <ilan.rivers@outlook.com>
2014-08-25 00:23:39 +0200

    Reverted example/crawl.php

--- example/crawl.php
+++ example/crawl.php
@@ -6,29 +6,16 @@ set_time_limit(0);
 use W4Y\Crawler\Client\Client;
 use W4Y\Crawler\Crawler;
 
-use W4Y\Crawler\Storage\MySQL;
-
-$dbFile = __DIR__ . '/sqllite.db';
-$storage = new MySQL('root', 'myDB10293847', 'testDB');
-
 
 $crawler = new Crawler;
 $crawler->setClient(new Client(), 'CrawlerClient 1');
 
-//$storage->reset();
-//$crawler->setStorage($storage);
-
 // Add URL to crawl
-$crawler->addToPending('http://dev.webb4you.com/sandbox/link_1.php');
-$crawler->addToPending('http://dev.webb4you.com/sandbox/link_6.php');
-
+$crawler->addToPending('http://en.wikipedia.org/wiki/Web_crawler');
 $crawler->crawl();
 
 $stats = $crawler->getClientStats();
 echo '<pre>' . print_r($stats, 1);
 
 $foundUrls = $crawler->getFoundUrls();
-echo '<pre>' . print_r($foundUrls, 1);
-
-$foundUrls = $crawler->getLastRequestData();
 echo '<pre>' . print_r($foundUrls, 1);
